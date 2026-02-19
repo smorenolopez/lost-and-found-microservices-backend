cd47133a4cdfdce88e99ee911794653357791e3b
Sergio Moreno López <sergio.moreno.lopez@pm.me>
2026-02-20 00:26:37 +0100

    Addition of gateway service

---
+++ users/src/main/java/com/smorenolopez/laf/users/controllers/UserController.java
@@ -0,0 +1,18 @@
+package com.smorenolopez.laf.users.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/status/check")
+    public ResponseEntity<String> check() {
+        return new ResponseEntity<>("Running", HttpStatus.OK);
+    }
+
+}
