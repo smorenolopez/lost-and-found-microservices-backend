d379ad93510c401b9ede52659e1a95fbbe91e7ea
Sergio Moreno López <sergio.moreno.lopez@pm.me>
2026-02-19 10:05:52 +0100

    Addition of laf users service

---
+++ LafUsers/src/test/java/com/smorenolopez/laf/users/lafusers/LafUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smorenolopez.laf.users.lafusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LafUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
