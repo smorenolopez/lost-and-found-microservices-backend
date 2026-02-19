19186339b712e662d486771bd766f1cfec5bccdf
Sergio Moreno López <sergio.moreno.lopez@pm.me>
2026-02-19 09:51:43 +0100

    Initial commit of laf discovery service

---
+++ LafDiscoveryService/src/test/java/com/smorenolopez/laf/discovery/lafdiscoveryservice/LafDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smorenolopez.laf.discovery.lafdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LafDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
